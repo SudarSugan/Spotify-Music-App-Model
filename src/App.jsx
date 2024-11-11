b601e376a027187f92913314900792f7d39933aa
Sudarsun <90299679+SudarSugan@users.noreply.github.com>
2024-11-11 09:28:15 +0530

    added app.jsx  file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from './Pages/NavBar'
 // import Home from './Pages/Home'
 import Footer from './Pages/Footer'
 import { Outlet } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 function App() {
   
@@ -11,7 +12,7 @@ function App() {
     <div>
     <NavBar />
     </div>
-
+    <SpeedInsights />
     <div>
       <Outlet/>
       
